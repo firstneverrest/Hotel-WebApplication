ef388a330beab3a60745770b0b35fa327d93e545
Chitsanupong <first30952@gmail.com>
2021-04-05 23:08:43 +0700

    explore page

--- hotel-collection/src/components/Explore/Explore.js
+++ hotel-collection/src/components/Explore/Explore.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import hotel1 from "../../img/img-hotel-1.jpg";
+import { IoSearch } from "react-icons/io5";
 
 const Explore = () => {
-    return (
-        <div>
-            This is Explore
-        </div>
-    )
-}
+  const [search, setSearch] = useState("");
 
-export default Explore
+  const searchChangeHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <div className="explore">
+      <h1 className="explore__title">Popular Thailand Hotel</h1>
+      <h4 className="explore__categories-title">Categories</h4>
+      <div className="explore__categories">
+        <div className="explore__categories-btn">
+          <img
+            className="explore__categories-image"
+            src={hotel1}
+            alt="Bangkok"
+          />
+          <p className="explore__categories-text">Bangkok</p>
+        </div>
+        <div className="explore__categories-btn">
+          <img
+            className="explore__categories-image"
+            src={hotel1}
+            alt="Bangkok"
+          />
+          <p className="explore__categories-text">Bangkok</p>
+        </div>
+        <div className="explore__categories-btn">
+          <img
+            className="explore__categories-image"
+            src={hotel1}
+            alt="Bangkok"
+          />
+          <p className="explore__categories-text">Bangkok</p>
+        </div>
+        <input
+          className="explore__search"
+          type="text"
+          value={search}
+          placeholder="search..."
+          onChange={searchChangeHandler}
+        />
+        <button className="explore__search-btn"><IoSearch /></button>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
